d59c57146af11a4c9c3310b1b12459c2117917a9
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-06-08 17:51:55 +0100

    api key

--- JSFolder/APIkey.js
+++ JSFolder/APIkey.js
@@ -2,6 +2,8 @@
  const APIkey = '06ac27c99b763f11a09533b2062845bb';
 
  
+ require('dotenv').config()
  export default APIkey;
 
 
+console.log(process.env)
---
+++ JSFolder/test.js
@@ -0,0 +1,3 @@
+const APIkey = '06ac27c99b763f11a09533b2062845bb';
+
+export default APIkey;
