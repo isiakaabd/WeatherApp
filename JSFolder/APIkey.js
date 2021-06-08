a77ca5bed08116b4094771d157e6ae598d38d385
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-06-08 17:56:14 +0100

    hiding API keys

--- JSFolder/APIkey.js
+++ JSFolder/APIkey.js
@@ -2,8 +2,8 @@
  const APIkey = '06ac27c99b763f11a09533b2062845bb';
 
  
- require('dotenv').config()
+//  require('dotenv').config()
  export default APIkey;
 
 
-console.log(process.env)
+// console.log(process.env)
